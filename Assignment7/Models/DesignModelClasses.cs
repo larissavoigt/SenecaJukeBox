575d83846c570c31433127e4f15a56a6c464e6ba
Larissa Voigt <hi@larissavoigt.com>
2016-03-18 18:23:03 -0700

    Add artist

--- Assignment7/Models/DesignModelClasses.cs
+++ Assignment7/Models/DesignModelClasses.cs
@@ -20,5 +20,25 @@ namespace Assignment7.Models
     // Do NOT configure scalar properties (e.g. int, double) with the [Required] attribute
     // Initialize DateTime and collection properties in a default constructor
 
+    public class Artist {
 
+        public Artist()
+        {
+            BirthOrStartDate = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+
+        public string BirthName { get; set; }
+
+        public DateTime BirthOrStartDate { get; set; }
+
+        public string Executive { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Name { get; set; }
+
+        public string UrlArtist { get; set; }
+    }
 }
