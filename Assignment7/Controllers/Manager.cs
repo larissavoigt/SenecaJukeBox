c42399b6c180311cf15cf5b7da0dba511c3e977c
Larissa Voigt <hi@larissavoigt.com>
2016-03-18 18:11:58 -0700

    Initial project structure

---
+++ Assignment7/Controllers/Manager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// new...
+using AutoMapper;
+using Assignment7.Models;
+
+namespace Assignment7.Controllers
+{
+    public class Manager
+    {
+        // Reference to the data context
+        private ApplicationDbContext ds = new ApplicationDbContext();
+
+        public Manager()
+        {
+            // If necessary, add constructor code here
+
+            // Turn off the Entity Framework (EF) proxy creation features
+            // We do NOT want the EF to track changes - we'll do that ourselves
+            ds.Configuration.ProxyCreationEnabled = false;
+
+            // Also, turn off lazy loading...
+            // We want to retain control over fetching related objects
+            ds.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // Add methods below
+        // Controllers will call these methods
+        // Ensure that the methods accept and deliver ONLY view model objects and collections
+        // The collection return type is almost always IEnumerable<T>
+
+        // Suggested naming convention: Entity + task/action
+        // For example:
+        // ProductGetAll()
+        // ProductGetById()
+        // ProductAdd()
+        // ProductEdit()
+        // ProductDelete()
+
+
+
+
+        // Attention - 13 - Add some programmatically-generated objects to the data store
+        // Can write one method, or many methods - your decision
+        // The important idea is that you check for existing data first
+        // Call this method from a controller action/method
+
+        public bool LoadData()
+        {
+            // Return if there's existing data
+
+            //if (ds.Your_Entity_Set.Count() > 0) { return false; }
+
+            // Otherwise...
+            // Create and add objects
+            // Save changes
+
+            return true;
+        }
+
+    }
+}
